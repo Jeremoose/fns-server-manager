aaa5b37b2876dd7c7a824317018838fe0a597756
jeremy <jeremy.vierbergen@moosetec.com>
2022-01-08 22:46:26 +0100

    add zeroed stdOut and stdErr

--- internal/app/fnsservermanager/server.go
+++ internal/app/fnsservermanager/server.go
@@ -22,7 +22,7 @@ func NewServer(name string, command string, arguments ...string) (*Server, error
 	if name == "" || command == "" {
 		return nil, errors.New("server name and/or command are not filled")
 	}
-	return &Server{name: name, command: command, arguments: arguments}, nil
+	return &Server{name: name, command: command, arguments: arguments, stdOut: new(bytes.Buffer), stdErr: new(bytes.Buffer)}, nil
 }
 
 func (s *Server) Run() error {
